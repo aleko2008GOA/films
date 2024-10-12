21a65bb04a9450dde97c70da5f3fba1129be2f0b
aleko2008GOA <alexmelikjanian@gmail.com>
2024-10-12 16:48:30 +0400

    new

--- src/getAPI.jsx
+++ src/getAPI.jsx
@@ -19,13 +19,14 @@ function GetFilmData(){
 
     return (
         <div>
+            <label style = {{fontSize: '20px', color: 'white', margin: '10px'}} htmlFor="movie">Search for a movie or film:</label>
             <input style = {{
                 border: '1px, solid',
                 fontSize: '20px',
                 borderRadius: '5px',
                 padding: '10px',
                 margin: '10px'
-            }} type = 'text' placeholder = 'Search for a movie' onChange = {handleChange} />
+            }} type = 'text' placeholder = 'Search for a movie' id = 'movie' onChange = {handleChange} />
             <FilmList movies = {data} />
         </div>
     )
